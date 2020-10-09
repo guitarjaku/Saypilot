9df73c6d644ae0860cf29b4a05a15c439d8f048b
G4RTU1 <g4rtu1@G4RTU1s-MacBook-Pro.local>
2020-10-09 09:09:36 +0700

    fix application pagr

--- pages/applications/index.tsx
+++ pages/applications/index.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import React from "react";
+import { h } from "preact";
 import { useState, useEffect } from "preact/hooks/";
 import Head from "next/head";
 import Header from "../../components/Header";
@@ -60,120 +58,6 @@ export default function Application() {
   return (
     <>
       <div className="h-screen flex overflow-hidden bg-white">
-        {/* <div className="lg:hidden">
-          <div className="fixed inset-0 flex z-40">
-            <div className="fixed inset-0">
-              <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
-            </div>
-
-            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-white">
-              <div className="absolute top-0 right-0 -mr-14 p-1">
-                <button
-                  className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
-                  aria-label="Close sidebar"
-                >
-                  <svg
-                    className="h-6 w-6 text-white"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  ></svg>
-                </button>
-              </div>
-              <div className="flex-shrink-0 flex items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src={"/trend-logo.svg"}
-                  alt="Workflow"
-                />
-              </div>
-
-              <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                <nav className="px-2">
-                  <div className="space-y-1">
-                    <a
-                      href="#"
-                      className="group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md text-gray-900 bg-gray-100 hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none transition ease-in-out duration-150"
-                    >
-                      <svg
-                        className="mr-3 h-6 w-6 text-gray-500 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      ></svg>
-                      Home
-                    </a>
-
-                    <a
-                      href="#"
-                      className="group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md text-gray-600 hover:text-gray-900 hover:bg-gray-50 focus:bg-gray-50 focus:outline-none transition ease-in-out duration-150"
-                    >
-                      <svg
-                        className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      ></svg>
-                      My tasks
-                    </a>
-
-                    <a
-                      href="#"
-                      className="group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md text-gray-600 hover:text-gray-900 hover:bg-gray-50 focus:bg-gray-50 focus:outline-none transition ease-in-out duration-150"
-                    >
-                      <svg
-                        className="mr-3 h-6 w-6 text-gray-400 group-hover:text-gray-500 group-focus:text-gray-600 transition ease-in-out duration-150"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      ></svg>
-                      Recent
-                    </a>
-                  </div>
-                  <div className="mt-8">
-                    <h3
-                      className="px-3 text-xs leading-4 font-semibold text-gray-500 uppercase tracking-wider"
-                      id="teams-headline"
-                    >
-                      Teams
-                    </h3>
-                    <div
-                      className="mt-1 space-y-1"
-                      role="group"
-                      aria-labelledby="teams-headline"
-                    >
-                      <a
-                        href="#"
-                        className="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150"
-                      >
-                        <span className="w-2.5 h-2.5 mr-4 bg-indigo-500 rounded-full"></span>
-                        <span className="truncate">Engineering</span>
-                      </a>
-
-                      <a
-                        href="#"
-                        className="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150"
-                      >
-                        <span className="w-2.5 h-2.5 mr-4 bg-teal-400 rounded-full"></span>
-                        <span className="truncate">Human Resources</span>
-                      </a>
-
-                      <a
-                        href="#"
-                        className="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition ease-in-out duration-150"
-                      >
-                        <span className="w-2.5 h-2.5 mr-4 bg-orange-500 rounded-full"></span>
-                        <span className="truncate">Customer Success</span>
-                      </a>
-                    </div>
-                  </div>
-                </nav>
-              </div>
-            </div>
-            <div className="flex-shrink-0 w-14"></div>
-          </div> 
-        </div>*/}
-
         {/* desktop */}
         <SideBar />
 
@@ -228,10 +112,7 @@ export default function Application() {
             <Head>
               <title>Applications | Saypilot</title>
               <meta property="og:title" content="Information about Genemator" />
-              <meta
-                property="og:description"
-                content="In this page you can get detailed information about Genemator and his works & experiences."
-              />
+              <meta property="og:description" content="" />
             </Head>
             <Header />
             <div className="p-4">
@@ -346,8 +227,9 @@ export default function Application() {
                             </tr>
                           </thead>
                           <tbody className="bg-white divide-y divide-gray-200">
-                            {influencers.map((influencer: any) => (
+                            {influencers.map((influencer: any, index: any) => (
                               <tr
+                                key={index}
                                 className="cursor-pointer hover:bg-gray-100"
                                 onClick={() => onOverSideOpen()}
                               >
